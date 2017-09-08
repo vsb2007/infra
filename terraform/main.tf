1cc91b5b71566aad253e865780554d5c4817ca25
Vladimir Baranov <330034@gmail.com>
2017-09-08 16:51:18 +0600

    terraform done

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,53 +1,67 @@
 provider "google" {
-	project = "infra-179306"
-	region = "europe-west1"
+  credentials = "${file(".terraform/account.json")}"
+  project     = "${var.project}"
+  region      = "${var.region}"
 }
+
 resource "google_compute_instance" "app" {
-	name = "reddit-app"
-	machine_type = "g1-small"
-	zone = "europe-west1-b"
-	tags = ["reddit-app"]
-	# определение загрузочного диска
-	boot_disk {
-		initialize_params {
-			image = "reddit-base-1504862704"
-		}
-	}
-	# определение сетевого интерфейса
-	network_interface {
-		# сеть, к которой присоединить данный интерфейс
-		network = "default"
-		# использовать ephemeral IP для доступа из Интернет
-		access_config {}
-	}
-	metadata {
-		sshKeys = "appuser:${file("~/.ssh/appuser.pub")}"
-	}
-	connection {
-		type = "ssh"
-		user = "appuser"
-		agent = false
-		private_key = "${file("~/.ssh/appuser")}"
-	}
-	provisioner "file" {
-		source = "files/puma.service"
-		destination = "/tmp/puma.service"
-	}
-	provisioner "remote-exec" {
-		script = "files/deploy.sh"
-	}
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "europe-west1-b"
+  tags         = ["reddit-app"]
+
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+
+  metadata {
+    sshKeys = "appuser:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
 }
+
 resource "google_compute_firewall" "firewall_puma" {
-	name = "allow-puma-default"
-	# Название сети, в которой действует правило
-	network = "default"
-	# Какой доступ разрешить
-	allow {
-		protocol = "tcp"
-		ports = ["9292"]
-	}
-	# Каким адресам разрешаем доступ
-	source_ranges = ["0.0.0.0/0"]
-	# Правило применимо для инстансов с тегом …
-	target_tags = ["reddit-app"]
+  name = "allow-puma-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с тегом …
+  target_tags = ["reddit-app"]
 }
