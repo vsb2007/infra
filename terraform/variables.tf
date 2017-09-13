1da4063238d7623c33e3ba413988395c4f4ce44f
Vladimir Baranov <330034@gmail.com>
2017-09-13 17:32:04 +0600

    terrafor separate main.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,3 +18,13 @@ variable disk_image {
 variable private_key_path {
   description = "Path to the private key used for ssh access"
 }
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
