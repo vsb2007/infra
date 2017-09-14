e58038ac6d47a19226df68b0ecb3d03af45d1d30
Vladimir Baranov <330034@gmail.com>
2017-09-14 09:28:52 +0600

    add modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,3 +4,18 @@ provider "google" {
   region      = "${var.region}"
 }
 
+module "app" {
+  source          = "modules/app"
+  public_key_path = "${var.public_key_path}"
+  app_disk_image  = "${var.app_disk_image}"
+}
+
+module "db" {
+  source          = "modules/db"
+  public_key_path = "${var.public_key_path}"
+  db_disk_image   = "${var.db_disk_image}"
+}
+
+module "vpc" {
+  source          = "modules/vpc"
+}
